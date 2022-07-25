f2648ef17997ca5f0bcaa506e4bc0fa9c5c9645f
Mina Milad Fahim <108484734+MinaMilad96@users.noreply.github.com>
2022-07-25 22:51:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let box1 = document.querySelector('.box1');
-let box2 = document.querySelector('.box2');
-let box3 = document.querySelector('.box3');
-let box4 = document.querySelector('.box4');
-let box5 = document.querySelector('.box5');
-let box6 = document.querySelector('.box6');
-let dark = document.querySelector('.dark');
-let button = document.querySelector('.button');
-let body = document.querySelector('#body');
 
-window.onscroll = function() {
-    "use strict";
-    let value = scrollY;
-    if (scrollY <= 1200){
-        box1.style.marginLeft = 300 + 'px';
-        box4.style.marginRight = 300 + 'px';
-    }
-}
 button.onclick = function(){
     "use strict";
     body.classList.toggle('dark');
